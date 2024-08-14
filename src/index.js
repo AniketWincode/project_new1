036b12ea384223c9e6b7944e8677968e78649fa6
Aniket <bedreaniket98@gmail.com>
2024-08-14 23:59:23 +0530

    schema

--- src/index.js
+++ src/index.js
@@ -6,17 +6,18 @@ const { connectDB } = require("./config/dbconfig")
 const app = express()
 
 // reading of data
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json()); // express.json()
+app.use(bodyParser.text()); // express.text()
+app.use(bodyParser.urlencoded());  express.urlencoded({extended : true})
 
 app.post('/ping', (req, res) => {
+    console.log("Hello")
     console.log(req.body);
     return res.json({message : "pong"})
 })
 
 app.listen(PORT, async () => {
-    console.log(`Server started at port ${PORT} ...!!`) 
+    console.log(`Server started at port ${PORT} ....!`) 
     await connectDB();
     console.log("Server started");
 });
---
+++ src/schema/userSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : [true, "First name is required"],
+        minLength : [5, "First must be atleast 5 chracter long"],
+        lowercase : true,
+        maxlength : [20, "First name should be less than or equal to 20 chracters"]
+    },
+
+    lastName : {
+        type : String,
+        required : [true, "LastName name is required"],
+        minLength : [5, "LastName must be atleast 5 chracter long"],
+        lowercase : true,
+        maxlength : [20, "LastName should be less than or equal to 20 chracters"]
+    },
+
+    mobileNumber : {
+        type : String,
+        trim : true,
+        maxlength : [10, "Phone number should be of length 10"],
+        minlength : [10, "Phone number should be of length 10"],
+        unique : [true, "Phone number is already in use"],
+        required : [true, "Phone number should be privided"]
+    },
+    
+    email : {
+        type : String,
+        trim : true,
+        required : [true, "Email should be provided"],
+        unique : [true, "Email is already in use"],
+        match : [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, P]
+    },
+    
+    password : {
+        type : String,
+        required : [true, "Password should be provided"],
+        minlength : [6, "Password should be minimum 6 chracter long"]
+    }
+}, {timestamps : true})
+
+const User = mongoose.model('User', userSchema) // covert to the collection
+
+module.exports =  {
+    User
+}
