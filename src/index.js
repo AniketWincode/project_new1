bcee2757b1e16059f0f2aed829453a8b68ef80b0
Aniket <bedreaniket98@gmail.com>
2024-08-14 21:59:14 +0530

    added bodyparser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const { PORT } = require("./config/serverConfig")
 const { connectDB } = require("./config/dbconfig")
 
 const app = express()
+
+// reading of data
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
+app.post('/ping', (req, res) => {
+    console.log(req.body);
+    return res.json({message : "pong"})
+})
 
 app.listen(PORT, async () => {
     console.log(`Server started at port ${PORT} ...!!`) 
     await connectDB();
     console.log("Server started");
 });
-
-
 
 // ambedreb21
 // gDs7BpJgxHPZWzWV
